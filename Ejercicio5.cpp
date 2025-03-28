f190fae3e00cbde81afb519d2a40136144ce714a
SamuelCarrascoRomero <carrascosamuel990@gmail.com>
2025-03-28 14:56:42 -0400

    Create Ejercicio5.cpp
    Termine

---
+++ Ejercicio5.cpp
@@ -0,0 +1,22 @@
+/******************************************************************************
+Ejercicio 5 
+Inicio
+    Declarar x, y como números de tipo doble
+    Leer x
+    Leer y
+    Imprimir la suma de x + y
+Fin
+
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main() {
+    double x, y=0;
+    cin >> x;
+    cin >> y;
+    cout << x + y << endl ;
+    
+    return 0;
+    }
